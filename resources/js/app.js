c43393efa5552de546b24199658273c33fb9088a
techbola <bfagbowore@gmail.com>
2019-03-26 04:41:48 +0100

    created two route links to users page and test page

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,39 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Users from './components/Users.vue'
+import Test from './components/Test.vue'
+
+import VueRouter from 'vue-router'
 import vueResource from 'vue-resource'
 
+Vue.use(VueRouter) 
 Vue.use(vueResource) 
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('test', require('./components/Test.vue').default);
-Vue.component('users', require('./components/Users.vue').default);
-
-const app = new Vue({
-    el: '#app'
+const router = new VueRouter({
+	mode: 'history',
+	base: __dirname,
+	routes: [
+		{
+			path: '/',
+			component: Users
+		},
+		{
+			path: '/test',
+			component: Test
+		}
+	]
 });
+
+new Vue({
+	router,
+    template: `
+		<div id="app">
+			<ul>
+				<li><router-link to="/">Users</router-link></li>
+				<li><router-link to="/test">Test</router-link></li>
+			</ul>
+			<router-view></router-view>
+		</div>
+    `
+}).$mount('#app');
